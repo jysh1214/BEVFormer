a0d3e5c30a48ede6b91bcdd5f7b63c38f14bd6ce
jysh1214 <jyxemperor@gmail.com>
2025-03-11 13:48:27 +0800

    Use cpu model

--- tools/test.py
+++ tools/test.py
@@ -227,7 +227,7 @@ def main():
 
     if not distributed:
         # assert False
-        model = MMDataParallel(model, device_ids=[0])
+        model.cpu()
         outputs = single_gpu_test(model, data_loader, args.show, args.show_dir)
     else:
         model = MMDistributedDataParallel(
